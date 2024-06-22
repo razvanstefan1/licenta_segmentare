ae24d38534293e12c323a99dc73b8a50755025c7
razvanstefan1 <116464592+razvanstefan1@users.noreply.github.com>
2024-06-22 21:49:13 +0300

    added comms to all classes

--- test.py
+++ test.py
@@ -12,96 +12,69 @@ from tqdm import tqdm
 import BatchDataReader
 from models.unet import UNet
 
+DATAROOT = 'C:\\Users\\brolz\\Desktop\\FACULTATE\\LICENTA\\STERGE'# Path to data
+GPU_IDS = '2'# GPU IDs
+TRAIN_IDS = [0, 240]# Train ID numbers
+VAL_IDS = [240, 250]# Validation ID numbers
+TEST_IDS = [250, 300]# Test ID numbers
+MODALITY_FILENAME = ['OCT(OPL_BM)', 'OCT(FULL)', 'OCT(ILM_OPL)', 'OCTA(OPL_BM)', 'OCTA(FULL)', 'OCTA(ILM_OPL)', 'GT_Multitask']# Dataset filenames, last name is label filename
+DATA_SIZE = [400, 400]# Input data size separated by comma
+IN_CHANNELS = 6# Input channels
+CHANNELS = 128# Channels  (each convolutional layer o sa aiba 128 de filtere gen kernel uri de alea carora le dai slide peste fiecare pixel)
+SAVEROOT = 'C:\\Users\\brolz\\Desktop\\FACULTATE\\LICENTA\\COD_LICENTA_SEGMENTARE\\saveroot'# Path to save results
+N_CLASSES = 5# Final class number for classification gen cate clase sa prezica (de la 0 la 4)
+MODE = 'test'# Mode (e.g., 'test', 'train')
 
-def parse_options():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--dataroot', default='C:\\Users\\brolz\\Desktop\\FACULTATE\\LICENTA\\STERGE',
-                        help='path to data')
-    parser.add_argument('--gpu_ids', type=str, default='2', help='gpu ids')
-    parser.add_argument('--train_ids', type=list, default=[0, 240], help='train id number')
-    parser.add_argument('--val_ids', type=list, default=[240, 250], help='val id number')
-    parser.add_argument('--test_ids', type=list, default=[250, 300], help='test id number')
-    parser.add_argument('--modality_filename', type=list,
-                        default=['OCT(OPL_BM)', 'OCT(FULL)', 'OCT(ILM_OPL)', 'OCTA(OPL_BM)', 'OCTA(FULL)',
-                                 'OCTA(ILM_OPL)', 'GT_Multitask'], help='dataset filename, last name is label filename')
-    parser.add_argument('--data_size', type=list, default=[400, 400], help='input data size separated with comma')
-    parser.add_argument('--in_channels', type=int, default=6, help='input channels')
-    parser.add_argument('--channels', type=int, default=128, help='channels')
-    parser.add_argument('--saveroot',
-                        default='C:\\Users\\brolz\\Desktop\\FACULTATE\\LICENTA\\COD_LICENTA_SEGMENTARE\\saveroot',
-                        help='path to save results')
-    parser.add_argument('--n_classes', type=int, default=5, help='final class number for classification')
-    parser.add_argument('--mode', type=str, default='test')
-
-    opt = parser.parse_args()
-    return opt
-
-
-def test_net(net, device, opt):
-    test_dataset = BatchDataReader.CubeDataset(opt.dataroot, opt.test_ids, opt.data_size, opt.modality_filename,
-                                               is_dataaug=False)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=1)
-    test_results = os.path.join(opt.saveroot, 'test_results')
-    BGR = np.zeros((opt.data_size[0], opt.data_size[1], 3))
-    net.eval()
+def test_net(net, device): #functia pentru testat netul
+    test_dataset = BatchDataReader.CubeDataset(DATAROOT, TEST_IDS, DATA_SIZE, MODALITY_FILENAME,is_dataaug=False) #obtinem un dataset pentru test folosind cubedataset
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=1) #se creaza un data loader cu batch size 1
+    test_results = os.path.join(SAVEROOT, 'test_results') #path ul unde se salveaza rezultatele testelor
+    BGR = np.zeros((DATA_SIZE[0], DATA_SIZE[1], 3)) #numpy array gol pentru  a stoca BGR data
+    net.eval()   #seteaza neural network ul sa fie pe evaluation mode
 
     # Test set
-    pbar = tqdm(enumerate(BackgroundGenerator(test_loader)), total=len(test_loader))
-    for itr, (test_images, test_annotations, cubename) in pbar:
-        test_images = test_images.to(device=device, dtype=torch.float32)
-        pred = net(test_images)
-        pred_argmax = torch.argmax(pred, dim=1)
-        pred_argmax = pred_argmax.cpu().detach().numpy()
-        print(cubename[0])
-        cv2.imwrite(os.path.join(test_results, cubename[0]), pred_argmax[0, :, :])
-        pred_softmax = torch.nn.functional.softmax(pred, dim=1)
-        pred_softmax = pred_softmax.cpu().detach().numpy()
-        BGR[:, :, 0] = 53 * pred_softmax[0, 0, :, :] + 143 * pred_softmax[0, 1, :, :] + 28 * pred_softmax[0, 2, :,
-                                                                                             :] + 186 * pred_softmax[0,
-                                                                                                        3, :,
-                                                                                                        :] + 106 * pred_softmax[
-                                                                                                                   0, 4,
-                                                                                                                   :, :]
-        BGR[:, :, 1] = 32 * pred_softmax[0, 0, :, :] + 165 * pred_softmax[0, 1, :, :] + 25 * pred_softmax[0, 2, :,
-                                                                                             :] + 131 * pred_softmax[0,
-                                                                                                        3, :,
-                                                                                                        :] + 217 * pred_softmax[
-                                                                                                                   0, 4,
-                                                                                                                   :, :]
-        BGR[:, :, 2] = 15 * pred_softmax[0, 0, :, :] + 171 * pred_softmax[0, 1, :, :] + 215 * pred_softmax[0, 2, :,
-                                                                                              :] + 43 * pred_softmax[0,
-                                                                                                        3, :,
-                                                                                                        :] + 166 * pred_softmax[
-                                                                                                                   0, 4,
-                                                                                                                   :, :]
-        cv2.imwrite(os.path.join(opt.saveroot, 'test_visuals', cubename[0]), BGR)
+    pbar = tqdm(enumerate(BackgroundGenerator(test_loader)), total=len(test_loader)) #progress bar, total este numarul de iteratii pentru progress bar
+    for itr, (test_images, test_annotations, cubename) in pbar: #iterates over each batch of test data, cubename e filenames for the current batch
+        test_images = test_images.to(device=device, dtype=torch.float32) #se muta imaginile de test pe device_ul specificat (cpu sau gpu)
+        pred = net(test_images) # obtine predictii de la network net(test_images) trece imaginile prin neural network pt a obtine predictiile
+        pred_argmax = torch.argmax(pred, dim=1) #se computeaza indexul valorii maxime pe dimensiunea 1, care corespunde clasei prezise (C A V F)
+        pred_argmax = pred_argmax.cpu().detach().numpy() #muta tensor ul (matricea) la CPU si o detaseaza de computation graph si o transf in numpy array
+        print(cubename[0]) #se printeaza filename al imaginii care e testata
+        cv2.imwrite(os.path.join(test_results, cubename[0]), pred_argmax[0, :, :]) #se salveaza predicted class map ca imagine
+        pred_softmax = torch.nn.functional.softmax(pred, dim=1) #aplica softmax pe predictii pentru a obtine class probabilities
+        pred_softmax = pred_softmax.cpu().detach().numpy() # muta softmax probabilities la cpu, le detaseaza de computation graph si le transforma in numpy array
+
+        #LINIILE DE MAI JOS OFERA SMOOTH TRANSITIONS LA CULORI IN FCT DE PROBABILITATI, de aia erau 10000 de culori diferite cand faceam histograma
+                    #softmax probabilities of class 0 are multiplied by 53, for class 1 by 143 and so on
+        BGR[:,:,0]=53*pred_softmax[0,0,:,:]+143*pred_softmax[0,1,:,:]+28*pred_softmax[0,2,:,:]+186*pred_softmax[0,3,:,:]+106*pred_softmax[0,4,:,:] #blue channel e creat combinand probabilitatile fiecarei clase, weighted cu coeficientii 53, 143, 28, 186, 106
+        BGR[:,:,1]=32*pred_softmax[0,0,:,:]+165*pred_softmax[0,1,:,:]+25*pred_softmax[0,2,:,:]+131*pred_softmax[0,3,:,:]+217*pred_softmax[0,4,:,:] # green channel e creat similar, cu coef diferiti 32, 165, 25, 131, 217
+        BGR[:,:,2]=15*pred_softmax[0,0,:,:]+171*pred_softmax[0,1,:,:]+215*pred_softmax[0,2,:,:]+43*pred_softmax[0,3,:,:]+166*pred_softmax[0,4,:,:] # red channel e creat similar cu coeficientii 15, 171, 215, 43, 166
+
+        cv2.imwrite(os.path.join(SAVEROOT, 'test_visuals', cubename[0]), BGR) #genereaza imaginea cu culori
 
 
 if __name__ == '__main__':
     # Setting logging
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
-    # Loading options
-    opt = parse_options()
     # Setting GPU
-    os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpu_ids
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    os.environ['CUDA_VISIBLE_DEVICES'] = GPU_IDS #restrictioneaza gpu urile vizibile la cele specificate
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') #daca e un gpu available il pune pe ala, daca nu, pe CPU
     logging.info(f'Using device {device}')
     # Loading network
-    net = UNet(in_channels=opt.in_channels, n_classes=opt.n_classes, channels=opt.channels)
+    net = UNet(in_channels=IN_CHANNELS, n_classes=N_CLASSES, channels=CHANNELS) #initializeaza o instanta a modelului cu input channels specificate, nr de clase specificate si nr de canale
     # net = AVNet(in_channels=opt.in_channels, n_classes=opt.n_classes, channels=opt.channels)
     # net = UNetpp(in_channels=opt.in_channels, n_classes=opt.n_classes, channels=opt.channels)
     # net = UNetppp(in_channels=opt.in_channels, n_classes=opt.n_classes, channels=opt.channels)
     # net = AttUNet(in_channels=opt.in_channels, n_classes=opt.n_classes, channels=opt.channels)
     # net = CSNet(in_channels=opt.in_channels, n_classes=opt.n_classes)
     # Load trained model
-    best_model_path = os.path.join(opt.saveroot, 'best_model',
-                                   natsort.natsorted(os.listdir(os.path.join(opt.saveroot, 'best_model')))[-1])
-    restore_path = os.path.join(best_model_path, os.listdir(best_model_path)[0])
-    net.load_state_dict(torch.load(restore_path, map_location=device))
+    best_model_path = os.path.join(SAVEROOT, 'best_model',natsort.natsorted(os.listdir(os.path.join(SAVEROOT, 'best_model')))[-1]) #face path pt best models si alege ultimul model directory dupa sortare (cel mai bun)
+    restore_path = os.path.join(best_model_path, os.listdir(best_model_path)[0]) #selecteaza path ul la primul fisier din best model directory
+    net.load_state_dict(torch.load(restore_path, map_location=device)) #loads model state from restore_path (path ul spre modelul efectiv care va fi folosit)
     # Input the model into GPU
-    net.to(device=device)
+    net.to(device=device) #muta modelul la device ul specificat pentru ca operatiile urmatoare sa fie efectuate pe acel device
     try:
-        test_net(net=net, device=device, opt=opt)
+        test_net(net=net, device=device)
     except KeyboardInterrupt:
         torch.save(net.state_dict(), 'INTERRUPTED.pth')
         logging.info('Saved interrupt')
--- train.py
+++ train.py
@@ -20,33 +20,24 @@ from torchsummary import summary
 import argparse
 
 
-def parse_options():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--dataroot', default='C:\\Users\\brolz\\Desktop\\FACULTATE\\LICENTA\\STERGE',
-                        help='path to data')
-    parser.add_argument('--gpu_ids', type=str, default='2', help='gpu ids')
-    parser.add_argument('--train_ids', type=list, default=[0, 240], help='train id number')
-    parser.add_argument('--val_ids', type=list, default=[240, 250], help='val id number')
-    parser.add_argument('--test_ids', type=list, default=[250, 300], help='test id number')
-    parser.add_argument('--modality_filename', type=list,
-                        default=['OCT(OPL_BM)', 'OCT(FULL)', 'OCT(ILM_OPL)', 'OCTA(OPL_BM)', 'OCTA(FULL)',
-                                 'OCTA(ILM_OPL)', 'GT_Multitask'], help='dataset filename, last name is label filename')
-    parser.add_argument('--data_size', type=list, default=[400, 400], help='input data size separated with comma')
-    parser.add_argument('--in_channels', type=int, default=6, help='input channels')
-    parser.add_argument('--channels', type=int, default=128, help='channels')
-    parser.add_argument('--saveroot',
-                        default='C:\\Users\\brolz\\Desktop\\FACULTATE\\LICENTA\\COD_LICENTA_SEGMENTARE\\saveroot',
-                        help='path to save results')
-    parser.add_argument('--n_classes', type=int, default=5, help='final class number for classification')
-    parser.add_argument('--load', type=str, default=False, help='whether restore or not')
-    parser.add_argument('--batch_size', type=int, default=4, help='input batch size')
-    parser.add_argument('--lr', type=float, default=0.0005, help='initial learning rate for adam')
-    parser.add_argument('--num_epochs', type=int, default=200, help='iterations for batch_size samples')
-    parser.add_argument('--mode', type=str, default='train')
-    parser.add_argument('--optimizer', type=str, default='Adam')
-
-    opt = parser.parse_args()
-    return opt
+# Global variables
+DATAROOT_PATH = 'C:\\Users\\brolz\\Desktop\\FACULTATE\\LICENTA\\STERGE'  # Path to data
+GPU_IDS = '2'  # GPU ids sa foloseasca al doilea GPU. Daca vrei mai multe le pui cu virgula  gen GPU_IDS = '0,1'
+TRAIN_IDS = [0, 240]  # Train id range
+VAL_IDS = [240, 250]  # Validation id range
+TEST_IDS = [250, 300]  # Test id range
+MODALITY_FILENAME = ['OCT(OPL_BM)', 'OCT(FULL)', 'OCT(ILM_OPL)', 'OCTA(OPL_BM)', 'OCTA(FULL)', 'OCTA(ILM_OPL)', 'GT_Multitask']  # Dataset filenames, last is label filename
+DATA_SIZE = [400, 400]  # Input data size
+IN_CHANNELS = 6  # Number of input channels
+CHANNELS = 128  # Number of channels
+SAVEROOT = 'C:\\Users\\brolz\\Desktop\\FACULTATE\\LICENTA\\COD_LICENTA_SEGMENTARE\\saveroot'  # Path to save results
+N_CLASSES = 5  # Number of final classes for classification
+LOAD = False  # Whether to load a pre-trained model
+BATCH_SIZE = 4  # Input batch size
+LEARNING_RATE = 0.0005  # Initial learning rate for Adam optimizer
+NUMBER_OF_EPOCHS = 200  # Number of epochs for training
+MODE = 'train'  # Mode (train/test)
+OPTIMIZER_TYPE = 'Adam'  # Optimizer type
 
 
 def calculate_miou(predicted_img, GT_img):
@@ -145,61 +136,61 @@ class DiceLoss(nn.Module):
         return total_loss / target.shape[1]
 
 
-def train_net(net, device, opt):
+def train_net(net, device): #antreneaza neural network ul cu device ul dat (cpu sau gpu)
     # train setting
-    val_num = opt.val_ids[1] - opt.val_ids[0]
-    best_valid_miou = 0
-    model_save_path = os.path.join(opt.saveroot, 'checkpoints')
-    best_model_save_path = os.path.join(opt.saveroot, 'best_model')
+    val_num = VAL_IDS[1] - VAL_IDS[0] #numarul de valori (cate imagini de validare o sa fie)
+    best_valid_miou = 0 #variabila care va contine best mean intersection over union score observed during validation
+    model_save_path = os.path.join(SAVEROOT, 'checkpoints') # construieste path pentru checkpoints
+    best_model_save_path = os.path.join(SAVEROOT, 'best_model') # construieste path pentru best model
     # Read Data
-    train_dataset = BatchDataReader.CubeDataset(opt.dataroot, opt.train_ids, opt.data_size, opt.modality_filename,
-                                                is_dataaug=False)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True, num_workers=4)
-    valid_dataset = BatchDataReader.CubeDataset(opt.dataroot, opt.val_ids, opt.data_size, opt.modality_filename,
-                                                is_dataaug=False)
-    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=4)
+    train_dataset = BatchDataReader.CubeDataset(DATAROOT_PATH, TRAIN_IDS, DATA_SIZE, MODALITY_FILENAME, is_dataaug=False) #creaza training dataset folosind CubeDataset
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4) #wrappuie datasetul de training intr un dataloader pentru a fi posibile batch processing, shuffling etc
+    valid_dataset = BatchDataReader.CubeDataset(DATAROOT_PATH, VAL_IDS, DATA_SIZE, MODALITY_FILENAME,is_dataaug=False) #creaza datasetul de validare
+    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=4) #wrappuie in data loader datasetul de validare
     # Setting Optimizer
-    if opt.optimizer == 'SGD':
-        optimizer = torch.optim.SGD(net.parameters(), opt.lr, momentum=0.9, weight_decay=1e-6)
-    elif opt.optimizer == 'Adam':
-        optimizer = torch.optim.Adam(net.parameters(), opt.lr, betas=(0.9, 0.99))
-    elif opt.optimizer == 'RMS':
-        optimizer = torch.optim.RMSprop(net.parameters(), opt.lr, weight_decay=1e-8)
+    if OPTIMIZER_TYPE == 'SGD': #initializeaza optimizatorul (optimizatorul updateaza iterativ weights urile modelului astfel incat sa se reduca loss ul)
+        optimizer = torch.optim.SGD(net.parameters(), LEARNING_RATE, momentum=0.9, weight_decay=1e-6)
+    elif OPTIMIZER_TYPE == 'Adam':
+        optimizer = torch.optim.Adam(net.parameters(), LEARNING_RATE, betas=(0.9, 0.99))
+    elif OPTIMIZER_TYPE == 'RMS':
+        optimizer = torch.optim.RMSprop(net.parameters(), LEARNING_RATE, weight_decay=1e-8)
     # Setting Loss
-    Loss_CE = nn.CrossEntropyLoss()
-    Loss_DSC = DiceLoss()
+    Loss_CE = nn.CrossEntropyLoss() #cross entropy loss function pentru classification tasks
+    Loss_DSC = DiceLoss() #Dice loss function pentru segmentation tasks
+
     # Start train
-    for epoch in range(1, opt.num_epochs + 1):
-        net.train()
-        pbar = tqdm(enumerate(BackgroundGenerator(train_loader)), total=len(train_loader))
-        for itr, (train_images, train_annotations, name) in pbar:
-            train_images = train_images.to(device=device, dtype=torch.float32)
-            train_annotations = train_annotations.to(device=device, dtype=torch.long)
-            pred = net(train_images)
-            loss = Loss_CE(pred, train_annotations) + 0.6 * Loss_DSC(pred, train_annotations)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+    for epoch in range(1, NUMBER_OF_EPOCHS + 1): #itereaza asupra numarului de epochs setate
+        net.train() #seteaza networkul la training mode
+        pbar = tqdm(enumerate(BackgroundGenerator(train_loader)), total=len(train_loader)) #wraps data loader in progres bar pentru a arata progressul
+        for itr, (train_images, train_annotations, name) in pbar: #itereaza peste fiecare batch de training data
+            train_images = train_images.to(device=device, dtype=torch.float32) #muta training images la cpu sau gpu (device)
+            train_annotations = train_annotations.to(device=device, dtype=torch.long) #muta labels urile la cpu sau gpu
+            pred = net(train_images) #passes the training images through the network to obtain predictions
+            loss = Loss_CE(pred, train_annotations) + 0.6 * Loss_DSC(pred, train_annotations) #calculeaza loss ul total ca o suma ponderata dintre cross entropy loss si dice loss
+            optimizer.zero_grad() #curata gradientii tuturor parametrilor optimizati pentru a preveni acumularea din pasi anteriori (iteratii anterioare cred)
+            loss.backward() #calculeaza gradientul loss-ului raportat la parametrii modelului folosind backpropagation
+            optimizer.step() #actualizeaza parametrii modelului bazat pe gradientii calculati
             # Start Val
-        with torch.no_grad():
+        with torch.no_grad(): #da disable la calcularea gradientilor pentru a  reduce consumul de memorie si a speedui up calculele din timpul validarilor
             # Save model
-            torch.save(net.module.state_dict(), os.path.join(model_save_path, f'{epoch}.pth'))
+            torch.save(net.module.state_dict(), os.path.join(model_save_path, f'{epoch}.pth')) #salveaza state dictionary ul modelului dupa fiecare epoch
             logging.info(f'Checkpoint {epoch} saved !')
             # Calculate validation mIOU
-            val_miou_sum = 0
-            net.eval()
-            pbar = tqdm(enumerate(BackgroundGenerator(valid_loader)), total=len(valid_loader))
-            for itr, (test_images, test_annotations, cubename) in pbar:
-                test_images = test_images.to(device=device, dtype=torch.float32)
-                test_annotations = test_annotations.cpu().detach().numpy()
-                pred = net(test_images)
-                pred_argmax = torch.argmax(pred, dim=1)
-                result = np.squeeze(pred_argmax).cpu().detach().numpy()
-                val_miou_sum += calculate_miou(result, test_annotations)
-            val_miou = val_miou_sum / val_num
+            val_miou_sum = 0 #initializeaza suma a miou de validare
+            net.eval() # si seteaza networkul in modul de evaluare
+            pbar = tqdm(enumerate(BackgroundGenerator(valid_loader)), total=len(valid_loader)) # progress bar pt validation
+            for itr, (test_images, test_annotations, cubename) in pbar: #itereaza fiece batch de validation data
+                test_images = test_images.to(device=device, dtype=torch.float32) #muta imaginile de test la device si converteste adnotarile la numpy array
+                test_annotations = test_annotations.cpu().detach().numpy() # muta labels la device si converteste adnotarile la numpy array
+                pred = net(test_images) #TRECE VALIDATION IMAGES PRIN NETWORK PENTRU A OBTINE PREDICTII
+                pred_argmax = torch.argmax(pred, dim=1) #calculeaza predicted class for each pixel
+                result = np.squeeze(pred_argmax).cpu().detach().numpy() #converteste predicted class la numpy array
+                val_miou_sum += calculate_miou(result, test_annotations) #acumuleaza miou pentru validation batch
+            val_miou = val_miou_sum / val_num #calculeaza average miou pentru validation set
             print("Step:{}, Valid_mIoU:{}".format(epoch, val_miou))
-            # save best model
-            if val_miou > best_valid_miou:
+
+            # save best model (daca validation miou al epoch ului curent este cel mai bun de pana acum, se salveaza modelul ca best model, se creaza un director cu numele
+            if val_miou > best_valid_miou:# fiind valoarea miou, se copiaza checkpoint ul la acest director. Se pastreaza doar ultimele 3 cele mai bune modele, stergand restul
                 temp = '{:.6f}'.format(val_miou)
                 os.mkdir(os.path.join(best_model_save_path, temp))
                 temp2 = f'{epoch}.pth'
@@ -214,30 +205,29 @@ if __name__ == '__main__':
     # setting logging
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
     # loading options
-    opt = parse_options()
     # setting GPU
-    os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpu_ids
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    os.environ['CUDA_VISIBLE_DEVICES'] = GPU_IDS #controleaza ce GPUs sunt vizibile pentru script
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') #vede daca cpu sau gpu
     logging.info(f'Using device {device}')
     # loading network
-    net = UNet(in_channels=opt.in_channels, n_classes=opt.n_classes, channels=opt.channels)
+    net = UNet(in_channels=IN_CHANNELS, n_classes=N_CLASSES, channels=CHANNELS) #initializeaza o instanta a unet-ului cu param specificati
     # net = AVNet(in_channels=opt.in_channels, n_classes=opt.n_classes, channels=opt.channels)
     # net = UNetpp(in_channels=opt.in_channels, n_classes=opt.n_classes, channels=opt.channels)
     # net = UNetppp(in_channels=opt.in_channels, n_classes=opt.n_classes, channels=opt.channels)
     # net = AttUNet(in_channels=opt.in_channels, n_classes=opt.n_classes, channels=opt.channels)
     # net = CSNet(in_channels=opt.in_channels, n_classes=opt.n_classes)
     # net=torch.nn.DataParallel(net,[0]).cuda()
-    net = torch.nn.DataParallel(net, [0])
+    net = torch.nn.DataParallel(net, [0]) #wrappuie modelul cu dataparellel pentru a enable-ui multi GPU training. Se specifica sa se foloseasca doar GPU cu id 0???????????
     # load trained model
-    if opt.load:
+    if LOAD: #incarca state dictionary ul unui model pre antrenat daca LOAD e setat la un path valid  dar e pus False acum
         net.load_state_dict(
-            torch.load(opt.load, map_location=device)
+            torch.load(LOAD, map_location=device)
         )
-        logging.info(f'Model loaded from {opt.load}')
+        logging.info(f'Model loaded from {LOAD}')
     # input the model into GPU
     # net.to(device=device)
     try:
-        train_net(net=net, device=device, opt=opt)
+        train_net(net=net, device=device)
     except KeyboardInterrupt:
         torch.save(net.state_dict(), 'INTERRUPTED.pth')
         logging.info('Saved interrupt')
