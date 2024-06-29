4ed975dcee479dcf35aaba19f07f41158584b005
razvanstefan1 <116464592+razvanstefan1@users.noreply.github.com>
2024-06-29 23:41:36 +0300

    am parametrizat randomforest.py si am adaugat metoda de classify

--- GUI/RandomForest/RandomForest.py
+++ GUI/RandomForest/RandomForest.py
@@ -1,151 +1,114 @@
-# import pandas as pd
-# from sklearn.model_selection import train_test_split
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.metrics import accuracy_score, classification_report
-# import warnings
-#
-# warnings.filterwarnings('ignore')
-# # Load the Titanic dataset
-# dataset_file_path = 'C:\\Users\\brolz\\Desktop\\FACULTATE\\LICENTA\\COD_LICENTA_SEGMENTARE\\GUI\\RandomForest\\Text_labels_and_Numerical_Data_no_surface_areas_csv.csv'
-# eye_measurements = pd.read_csv(dataset_file_path)
-# print(eye_measurements.columns)
-#
-#
-# # Drop rows with missing target values
-# eye_measurements = eye_measurements.dropna(subset=['Disease'])
-#
-# # Create a binary target variable for DR
-# eye_measurements['DR'] = eye_measurements['Disease'].apply(lambda x: 1 if x == 'DR' else 0)
-#
-# # Select relevant features and target variable
-# X = eye_measurements[['Age', 'FAZ_circularity','Art_density', 'Cap_density','LV_density','Vein_density','Artery_diameter_index','Capillary_diameter_index','Vein_diameter_index','LargeVessel_diameter_index' ]]
-#
-# #Target variable (variabila ce vrem s o determinam)
-# y = eye_measurements['DR']
-#
-# # Split the dataset into training and testing sets
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=12)
-# # Create a Random Forest Classifier with 100 trees (estimators) and a random seed of 42
-# rf_classifier = RandomForestClassifier(n_estimators=10000, random_state=12, class_weight={0: 1, 1: 10})
-#
-# # Train the classifier
-# rf_classifier.fit(X_train, y_train)
-#
-# # Make predictions on the test set
-# y_pred = rf_classifier.predict(X_test)
-#
-# # Evaluate the model
-# accuracy = accuracy_score(y_test, y_pred)
-# classification_rep = classification_report(y_test, y_pred, target_names=['Not DR', 'DR'])
-#
-# # Print the results
-# print(f"Accuracy: {accuracy:.2f}")
-# print("\nClassification Report:\n", classification_rep)
-import numpy as np
-# import pandas as pd
-# from sklearn.model_selection import train_test_split
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.metrics import accuracy_score, classification_report
-# import warnings
-#
-# warnings.filterwarnings('ignore')
-# # Load the Titanic dataset
-# dataset_file_path = 'C:\\Users\\brolz\\Desktop\\FACULTATE\\LICENTA\\COD_LICENTA_SEGMENTARE\\GUI\\RandomForest\\Text_labels_and_Numerical_Data_no_surface_areas_csv.csv'
-# eye_measurements = pd.read_csv(dataset_file_path)
-# print(eye_measurements.columns)
-#
-#
-# # Drop rows with missing target values
-# eye_measurements = eye_measurements.dropna(subset=['Disease'])
-#
-# # Create a binary target variable for DR
-# eye_measurements['AMD'] = eye_measurements['Disease'].apply(lambda x: 1 if x == 'AMD' else 0)
-#
-# # Select relevant features and target variable
-# X = eye_measurements[['Age', 'FAZ_circularity','Art_density', 'Cap_density','LV_density','Vein_density','Artery_diameter_index','Capillary_diameter_index','Vein_diameter_index','LargeVessel_diameter_index' ]]
-#
-# #Target variable (variabila ce vrem s o determinam)
-# y = eye_measurements['AMD']
-#
-# # Split the dataset into training and testing sets
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2)
-# # Create a Random Forest Classifier with 100 trees (estimators) and a random seed of 42
-# rf_classifier = RandomForestClassifier(n_estimators=5000, random_state=2)
-#
-# # Train the classifier
-# rf_classifier.fit(X_train, y_train)
-#
-# # Make predictions on the test set
-# y_pred = rf_classifier.predict(X_test)
-#
-# # Evaluate the model
-# accuracy = accuracy_score(y_test, y_pred)
-# classification_rep = classification_report(y_test, y_pred, target_names=['Not AMD', 'AMD'])
-#
-# # Print the results
-# print(f"Accuracy: {accuracy:.2f}")
-# print("\nClassification Report:\n", classification_rep)
-
-
-
-
-
-
+import graphviz
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 import warnings
+import numpy as np
+from sklearn.tree import export_graphviz
 
-warnings.filterwarnings('ignore')
-# Load the Titanic dataset
-dataset_file_path = 'C:\\Users\\brolz\\Desktop\\FACULTATE\\LICENTA\\COD_LICENTA_SEGMENTARE\\GUI\\RandomForest\\Text_labels_and_Numerical_Data_no_surface_areas_csv.csv'
-eye_measurements = pd.read_csv(dataset_file_path)
-print(eye_measurements.columns)
+#conceput pentru clasificare binara
+#e tot fisier care poate fi rulat de sine statator dar metodele lui pot fi apelate si din alte fisiere (ca si GenerateSegmentation.py)
+
+def GenerateTrainTestRForest(csv_path, target_column, target_disease, feature_list, estimator_count, seed_number, percentage_test_data, show_importance_graph=False, show_tree_visualization=False):
+    #citim csv ul
+    eye_measurements = pd.read_csv(csv_path)
+    print(eye_measurements.columns)
+
+    #scoatem coloana care contine targetul
+    eye_measurements = eye_measurements.dropna(subset=[target_column])
+
+    #alegem boala pe care facem clasificarea binara (randurile cu aceasta boala iau valoarea 1, restul 0)
+    eye_measurements[target_disease] = eye_measurements[target_column].apply(lambda x: 1 if x == target_disease else 0)
+
+    #alegem feature-urile
+    X=eye_measurements[feature_list]
+
+    #boala target
+    y=eye_measurements[target_disease]
+
+    #splittuim datasetul in train si test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=percentage_test_data, random_state=seed_number)
+
+    #creem un random forest classifier
+    random_forest_classifier = RandomForestClassifier(n_estimators=estimator_count, random_state=seed_number)
+
+    #antrenam clasificatorul
+    random_forest_classifier.fit(X_train, y_train)
+
+    #facem predictii pe setul de test
+    y_pred = random_forest_classifier.predict(X_test)
+
+    #evaluare model
+    accuracy = accuracy_score(y_test, y_pred)
+    metrics_report = classification_report(y_test, y_pred, target_names=[f'Not {target_disease}', f'{target_disease}'])
+
+    #printare
+    print(f"Accuracy: {accuracy:.2f}")
+    print("\nClassification Report:\n", metrics_report)
+
+    #generare grafic cu importanta feature-urilor
+    if show_importance_graph:
+        generateFeatureImportanceGraph(random_forest_classifier, feature_list, X)
+
+    #generare vizualizare arbore
+    if show_tree_visualization:
+        generateTreeVisualization(random_forest_classifier, feature_list, [f'Not {target_disease}', f'{target_disease}'])
+
+    return random_forest_classifier
 
 
-# Drop rows with missing target values
-eye_measurements = eye_measurements.dropna(subset=['Disease'])
+def generateFeatureImportanceGraph(RF, feature_list, X):
+    importances = RF.feature_importances_
+    indices = np.argsort(importances)[::-1]
+    # Plot feature importances
+    plt.figure(figsize=(10, 6))
+    plt.title("Feature Importances")
+    plt.bar(range(X.shape[1]), importances[indices], align='center')
+    plt.xticks(range(X.shape[1]), [feature_list[i] for i in indices], rotation=90)
+    plt.tight_layout()
+    plt.show()
 
-# Create a binary target variable for DR
-eye_measurements['NORMAL'] = eye_measurements['Disease'].apply(lambda x: 1 if x == 'NORMAL' else 0)
+def generateTreeVisualization(RF, feature_list, target_names_list):
+    # GENERAM O vizualizare a primului arbore din forest (pentru a intelege cum se iau deciziile). se genereaza fisier dot si pdf
+    tree = RF.estimators_[0] #etragem primul arbore din forest
+    export_graphviz(tree, out_file='tree.dot', feature_names=feature_list, class_names=target_names_list, filled=True)
 
-# Select relevant features and target variable
-features = ['Age', 'FAZ_circularity', 'Art_density', 'Cap_density', 'LV_density', 'Vein_density', 'Artery_diameter_index', 'Capillary_diameter_index', 'Vein_diameter_index', 'LargeVessel_diameter_index']
-X = eye_measurements[features]
-#Target variable (variabila ce vrem s o determinam)
-y = eye_measurements['NORMAL']
+    # citim fisierul dot
+    with open('tree.dot') as f:
+        dot_graph = f.read()
 
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
-# Create a Random Forest Classifier with 100 trees (estimators) and a random seed of 42
-rf_classifier = RandomForestClassifier(n_estimators=5000, random_state=123)
-
-# Train the classifier
-rf_classifier.fit(X_train, y_train)
-
-# Make predictions on the test set
-y_pred = rf_classifier.predict(X_test)
-
-# Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-classification_rep = classification_report(y_test, y_pred, target_names=['Not healthy', 'healthy'])
-
-# Print the results
-print(f"Accuracy: {accuracy:.2f}")
-print("\nClassification Report:\n", classification_rep)
+    graph = graphviz.Source(dot_graph)
+    graph.render('tree')
+    graph.view()
 
 
+def GenerateClassificationResult(value_row, feature_list, target_disease, RF):
+    #creem un dataframe cu valorile care vor fi clasificate
+    property_data_frame = pd.DataFrame([value_row], columns=feature_list)
 
-#GENEREAZA GRAFICUL CU Importanta features-urilor
-importances = rf_classifier.feature_importances_
-indices = np.argsort(importances)[::-1]
+    #facem predictia
+    prediction = RF.predict(property_data_frame)
 
-# Plot feature importances
-plt.figure(figsize=(10, 6))
-plt.title("Feature Importances")
-plt.bar(range(X.shape[1]), importances[indices], align='center')
-plt.xticks(range(X.shape[1]), [features[i] for i in indices], rotation=90)
-plt.tight_layout()
-plt.show()
+    #printam rezultatul predictiei
+    if prediction[0] == 1:
+        print(f'Classification result: {target_disease}')
+    else:
+        print(f'Classification result: NOT {target_disease}')
+
+    return prediction[0]
+
+if __name__ == '__main__':
+    csv_path = 'C:\\Users\\brolz\\Desktop\\FACULTATE\\LICENTA\\COD_LICENTA_SEGMENTARE\\GUI\\RandomForest\\Text_labels_and_Numerical_Data_no_surface_areas_csv.csv'
+    target_column = 'Disease'
+    target_disease = 'NORMAL'
+    feature_list = ['Age', 'FAZ_circularity', 'Art_density', 'Cap_density', 'LV_density', 'Vein_density', 'Artery_diameter_index', 'Capillary_diameter_index', 'Vein_diameter_index', 'LargeVessel_diameter_index']
+    estimator_count = 5000
+    seed_number = 123
+    percentage_test_data = 0.2
+
+    RF=GenerateTrainTestRForest(csv_path, target_column, target_disease, feature_list, estimator_count, seed_number, percentage_test_data, show_importance_graph=True, show_tree_visualization=True)
+
+    #print('generare')
+    #GenerateClassificationResult([55,0.459722275,0.0390625,0.46563125,0.08276875,0.043975,8.933119744,6.331678888,8.935320228,8.899184149], feature_list, target_disease, RF)
